45cf41cf0dd3fc2efc7dffe6d5b8629d0ddfa462
Amulya Dixit <amulyadixit07@gmail.com>
2021-07-16 23:52:22 +0530

    setting up the Layout

--- server.js
+++ server.js
@@ -8,16 +8,19 @@ const PORT = process.env.PORT || 3000;
 //Assets
 app.use(express.static('public'));
 
-app.get('/',function(req,res){
-    res.render("home");
-})
-
 //set Template engine
 app.use(expressLayout);
 //setting views path
 app.set('views', path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
 
+app.get('/',function(req,res){
+    res.render("home");
+})
+
+app.get('/cart', function(req,res){
+    res.render("Customers/cart");
+})
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT} `);
