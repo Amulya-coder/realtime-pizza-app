69826b0f12385f30b20791336feee7ebb305ae34
Amulya Dixit <amulyadixit07@gmail.com>
2021-07-31 13:43:27 +0530

    connect-mongo for session store

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const dotenv=require('dotenv').config();
 const express=require('express');
 const app = express();
 const ejs=require('ejs');
 const path=require('path');
 const expressLayout=require('express-ejs-layouts');
 const mongoose=require('mongoose');
+const session=require('express-session');
+const flash=require('express-flash');
+const MongoDbStore=require('connect-mongo');
 const PORT = process.env.PORT || 3000;
 
 //Database connection
@@ -17,6 +21,25 @@ connection.once('open',()=>{
 console.log('Connection failed...');
 });
 
+//Session store
+// let mongoStore= new MongoDbStore({
+//     mongooseConnection:connection,
+//     collection:'sessions'
+// })
+
+//Session Config
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave:false,
+    store:MongoDbStore.create({
+        client:connection.getClient()
+    }),
+    saveUninitialized:false,
+    cookie:{maxAge:1000*60*60*24} //24 hours
+
+}))
+
+app.use(flash())
 //Assets
 app.use(express.static('public'));
 
